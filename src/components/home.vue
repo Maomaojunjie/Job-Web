f74e6ed101f66c87b9fbfca0bfafb32afa62b278
junjie <junjie@kooboo.net>
2021-08-01 16:51:29 +0800

    feat: Initialize the project

---
+++ src/components/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    求职招聘网站--HMY
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <home />
+</template>
+
+<script>
+import Home from '@components/home.vue'
+export default {
+  components: {
+    Home
+  }
+}
+</script>
